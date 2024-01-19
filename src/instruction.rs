982857fcbf986387509f0648856a3594e1558fb0
Emmanuel Oluwatobi <56241351+Tee-py@users.noreply.github.com>
2024-01-19 14:29:57 +0100

    added presale instruction

--- src/instruction.rs
+++ src/instruction.rs
@@ -1,10 +1,41 @@
 use std::convert::TryInto;
 use solana_program::program_error::ProgramError;
 use crate::error::EscrowError::InvalidInstruction;
+use arrayref::array_refs;
 
 pub enum PresaleInstruction {
-    BuyToken {
-        amount: u64
+    InitPresale {
+        start_timestamp: u64,
+        token_price: u64
+    }
+}
+
+impl PresaleInstruction {
+    pub fn unpack(input: &[u8]) -> Result<Self, ProgramError> {
+        let (tag, rest) = input.split_first().ok_or(InvalidInstruction)?;
+        let (
+            _,
+            start_ts_arr,
+            token_price_arr,
+        ) = array_refs![input, 1, 8, 8];
+        Ok(
+            match tag {
+                0 => Self::InitPresale {
+                    start_timestamp: Self::unpack_u64(start_ts_arr)?,
+                    token_price: Self::unpack_u64(token_price_arr)?
+                },
+                _ => return Err(InvalidInstruction.into()),
+            }
+        )
+    }
+
+    fn unpack_u64(input: &[u8]) -> Result<u64, ProgramError> {
+        let value = input
+            .get(..8)
+            .and_then(|slice| slice.try_into().ok())
+            .map(u64::from_le_bytes)
+            .ok_or(InvalidInstruction)?;
+        Ok(value)
     }
 }
 
