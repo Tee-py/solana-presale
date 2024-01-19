1c4992e3a3d66aa3349d842d885af3773a890856
Emmanuel Oluwatobi <56241351+Tee-py@users.noreply.github.com>
2024-01-19 17:29:09 +0100

    added buy token processor function

--- src/processor.rs
+++ src/processor.rs
@@ -6,8 +6,10 @@ use solana_program::{
     pubkey::Pubkey,
     program_pack::{Pack, IsInitialized},
     sysvar::{rent::Rent, Sysvar},
-    program::invoke
+    system_instruction,
+    program::{invoke, invoke_signed}
 };
+use spl_token::state::{Account as TokenAccount, Mint};
 use solana_program::clock::Clock;
 use crate::{instruction::{PresaleInstruction}, error::{PresaleError}, state::Presale};
 
@@ -21,7 +23,10 @@ impl Processor {
             PresaleInstruction::InitPresale { start_timestamp, token_price } => {
                 msg!("Instruction: InitPresale");
                 Self::process_init_presale(accounts, start_timestamp, token_price, program_id)
-            }
+            },
+            PresaleInstruction::BuyToken { amount } => {
+                msg!("Instruction: BuyToken");
+            },
         }
     }
 
@@ -77,79 +82,66 @@ impl Processor {
         Ok(())
     }
 
-    // fn process_init_escrow(
-    //     accounts: &[AccountInfo],
-    //     amount: u64,
-    //     program_id: &Pubkey
-    // ) -> ProgramResult {
-    //     // let cl = Clock::get()?;
-    //     // let ts = cl.unix_timestamp.to_be_bytes();
-    //     let account_info_iter = &mut accounts.iter();
-    //     let initializer = next_account_info(account_info_iter)?;
-    //
-    //     if !initializer.is_signer {
-    //         return Err(ProgramError::MissingRequiredSignature);
-    //     }
-    //
-    //     let temp_token_account = next_account_info(account_info_iter)?;
-    //
-    //     let token_to_receive_account = next_account_info(account_info_iter)?;
-    //     if *token_to_receive_account.owner != spl_token::id() {
-    //         return Err(ProgramError::IncorrectProgramId);
-    //     }
-    //
-    //     let escrow_account = next_account_info(account_info_iter)?;
-    //     let rent = &Rent::from_account_info(next_account_info(account_info_iter)?)?;
-    //
-    //     if !rent.is_exempt(escrow_account.lamports(), escrow_account.data_len()) {
-    //         return Err(EscrowError::NotRentExempt.into());
-    //     }
-    //
-    //     let mut escrow_info = Escrow::unpack_unchecked(&escrow_account.data.borrow())?;
-    //     if escrow_info.is_initialized() {
-    //         return Err(ProgramError::AccountAlreadyInitialized);
-    //     }
-    //
-    //     escrow_info.is_initialized = true;
-    //     escrow_info.initializer_pubkey = *initializer.key;
-    //     escrow_info.temp_token_account_pubkey = *temp_token_account.key;
-    //     escrow_info.initializer_token_to_receive_account_pubkey = *token_to_receive_account.key;
-    //     escrow_info.expected_amount = amount;
-    //
-    //     Escrow::pack(escrow_info, &mut escrow_account.try_borrow_mut_data()?)?;
-    //
-    //     let (pda, _bump_seed) = Pubkey::find_program_address(&[b"escrow"], program_id);
-    //
-    //     let token_program = next_account_info(account_info_iter)?;
-    //     let owner_change_ix = spl_token::instruction::set_authority(
-    //         token_program.key,
-    //         temp_token_account.key,
-    //         Some(&pda),
-    //         spl_token::instruction::AuthorityType::AccountOwner,
-    //         initializer.key,
-    //         &[&initializer.key],
-    //     )?;
-    //
-    //     msg!("Calling the token program to transfer token account ownership...");
-    //     invoke(
-    //         &owner_change_ix,
-    //         &[
-    //             temp_token_account.clone(),
-    //             initializer.clone(),
-    //             token_program.clone(),
-    //         ]
-    //     )?;
-    //     Ok(())
-    // }
+    fn process_buy_token(
+        accounts: &[AccountInfo],
+        amount_in_sol: u64,
+        program_id: &Pubkey
+    ) -> ProgramResult {
+        let account_info_iter = &mut accounts.iter();
+        let buyer = next_account_info(account_info_iter)?;
 
-    // pub fn process(program_id: &Pubkey, accounts: &[AccountInfo], instruction_data: &[u8]) -> ProgramResult {
-    //     let instruction = EscrowInstruction::unpack(instruction_data)?;
-    //
-    //     match instruction {
-    //         EscrowInstruction::InitEscrow { amount } => {
-    //             msg!("Instruction: InitEscrow");
-    //             Self::process_init_escrow(accounts, amount, program_id)
-    //         }
-    //     }
-    // }
+        if !buyer.is_signer {
+            return Err(ProgramError::MissingRequiredSignature);
+        }
+
+        // Gets all the accounts and account info needed for this method
+        let buyer_presale_token_account = next_account_info(account_info_iter)?;
+        let pda_token_account = next_account_info(account_info_iter)?;
+        let pda_token_account_info = TokenAccount::unpack(&pda_token_account.try_borrow_data()?)?;
+        let (pda, bump_seed) = Pubkey::find_program_address(&[b"presale"], program_id);
+        let presale_owner_account = next_account_info(account_info_iter)?;
+        let presale_account = next_account_info(account_info_iter)?;
+        let presale_info = Presale::unpack(&presale_account.try_borrow_data()?)?;
+        let presale_token_program = next_account_info(account_info_iter)?;
+        let pda_account = next_account_info(account_info_iter)?;
+
+        // Performs necessary checks
+        if presale_info.token_account_pubkey != *pda_token_account.key {
+            return Err(PresaleError::InvalidPresaleTokenAccount.into());
+        }
+        if presale_info.owner_pubkey != *presale_owner_account.key {
+            return Err(PresaleError::InvalidPresaleOwnerAccount.into());
+        }
+        if buyer.lamports() < amount_in_sol {
+            return Err(ProgramError::InsufficientFunds);
+        }
+        // Transfers solana to the presale owner
+        solana_program::program::invoke_signed(
+            &system_instruction::transfer(buyer.key, presale_owner_account.key, amount_in_sol),
+            &[buyer.clone()],
+            &[]
+        )?;
+        // Transfer presale token to buyer
+        let presale_token_mint = Mint::unpack(&pda_token_account.data.borrow())?;
+        let amount_out = amount_in_sol * presale_info.token_price * (10u64.pow(presale_token_mint.decimals as u32));
+        let token_transfer_ix = spl_token::instruction::transfer(
+            presale_token_program.key,
+            pda_token_account.key,
+            buyer_presale_token_account.key,
+            &pda,
+            &[&pda],
+            amount_out
+        )?;
+        invoke_signed(
+            &token_transfer_ix,
+            &[
+                pda_token_account.clone(),
+                buyer_presale_token_account.clone(),
+                pda_account.clone(),
+                presale_token_program.clone(),
+            ],
+            &[&[&b"escrow"[..], &[bump_seed]]],
+        )?;
+        Ok(())
+    }
 }
