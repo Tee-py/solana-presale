21f36442b0350cfcd978bf76d7516da3b2124a4f
Emmanuel Oluwatobi <56241351+Tee-py@users.noreply.github.com>
2024-01-19 11:47:02 +0100

    initialized project

---
+++ src/lib.rs
@@ -0,0 +1,20 @@
+pub mod entrypoint;
+pub mod error;
+pub mod instruction;
+pub mod processor;
+pub mod state;
+
+pub fn add(left: usize, right: usize) -> usize {
+    left + right
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
